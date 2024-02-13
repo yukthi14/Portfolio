25d598cde44064efaf167c718c09ab83fcee401c
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-13 21:04:32 +0530

    Color file added.

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color bgColor = const Color(0xff003153);
+  static Color themeColor = const Color(0xff007BA7);
+  static Color splashColor = const Color(0xFF84FFFF);
+  static Color borderColor = const Color(0xFF18FFFF);
+  static Color white = Colors.white;
+  static Color lightBlue = Colors.lightBlue;
+  static Color lightShadeBlue = Colors.lightBlue.shade200;
+}
