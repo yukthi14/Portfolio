38705501c7a8d35f8f57e8959cca56b7436fee1f
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-13 21:05:13 +0530

    Constant size box for providing spaces in between the conatiner or text.

---
+++ lib/constants/const_sized_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Constants {
+  static SizedBox sizedBox({height, width}) {
+    return SizedBox(
+      height: height,
+      width: width,
+    );
+  }
+}
