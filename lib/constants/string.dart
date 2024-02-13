f3941a07d3c54fcbc540e30895127dc1659bba59
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-13 21:08:17 +0530

    new strings added.

--- lib/constants/string.dart
+++ lib/constants/string.dart
@@ -1,35 +1,61 @@
 import 'package:portfolio/constants/url.dart';
 
 class Strings {
+  static const String portfolio = "Portfolio";
+  static const String home = "Home";
+  static const String about = "About";
+  static const String services = "Services";
+  static const String contact = "Contact";
+  static const String helloText = "Hello, It\'s Me";
+  static const String role = "And I\'m a ";
+  static const String downloadCv = "Download CV";
 
-static const  String name = "Yukthi J Achar";
-static const  String username = "@yukthi";
-static const String contactEmail = "yukthiachar@gmail.com";
-static const String aboutMeSummary = '''
+  static const String name = "Yukthi J Achar";
+  static const String username = "@yukthi";
+  static const String contactEmail = "yukthiachar@gmail.com";
+  static const String aboutMeSummary = '''
 * Currently an engineering student with full of enthusiasm and leadership quality, good at problem solving .
 * I easily get adopted to the environment and workspace.
 * Some of the programming languages which i know are dart(flutter) , java , c , c ++ , sql , python .
 * Strong in flutter which uses dart programming, I have done many projects related to flutter in app and website.
 * In intermediate level comes java, all other languages i have a good knowledge about the syntax and usage.
 ''';
-static const String studentContact= "Student Contact";
-static const String stuConDes="Contact app using flutter, sql lite and firebase.";
+  static const String studentContact = "Student Contact";
+  static const String stuConDes =
+      "Contact app using flutter, sql lite and firebase.";
 
-static const String dataStructure= "Data Structure";
-static const String datStuDes="Simple listview with data structure program.";
+  static const String dataStructure = "Data Structure";
+  static const String datStuDes =
+      "Simple listview with data structure program.";
 
-static const String calculator= "Calculator";
-static const String calDes="Calculator operations and functions.";
+  static const String calculator = "Calculator";
+  static const String calDes = "Calculator operations and functions.";
 
-static const String blocConnection= "Bloc Connection";
-static const String blocDes="How internet connection takes place using block.";
+  static const String blocConnection = "Bloc Connection";
+  static const String blocDes =
+      "How internet connection takes place using block.";
 
-static const String errorMessage="Something went wrong !!!!!!!!!!";
-static const String mailTo="Mail To : yukthiachar@gmail.com ";
-
+  static const String errorMessage = "Something went wrong !!!!!!!!!!";
+  static const String mailTo = "Mail To : yukthiachar@gmail.com ";
 }
+
 class Lists {
-  static const List projectNameLists=[Strings.studentContact,Strings.dataStructure,Strings.calculator,Strings.blocConnection];
-  static const List projectDescLists=[Strings.stuConDes,Strings.datStuDes,Strings.calDes,Strings.blocDes];
-  static const List projectLinkLists=[Links.studentContactLink,Links.dataStructureLink,Links.calculatorLink,Links.blocConnectionLink];
+  static const List projectNameLists = [
+    Strings.studentContact,
+    Strings.dataStructure,
+    Strings.calculator,
+    Strings.blocConnection
+  ];
+  static const List projectDescLists = [
+    Strings.stuConDes,
+    Strings.datStuDes,
+    Strings.calDes,
+    Strings.blocDes
+  ];
+  static const List projectLinkLists = [
+    Links.studentContactLink,
+    Links.dataStructureLink,
+    Links.calculatorLink,
+    Links.blocConnectionLink
+  ];
 }
