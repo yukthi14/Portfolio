1e65038f0c0a4d169ea65fe11f224f9acf00e0ae
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-21 22:23:45 +0530

    profile image and amc logo link added in appAssets.

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -2,4 +2,6 @@ class AppAssets {
   static String github = 'assets/git.png';
   static String linkedIn = 'assets/in.png';
   static String twitter = 'assets/twit.png';
+  static String profileImage = 'assets/profile.jpg';
+  static String amcLogo = 'assets/amc_logo.png';
 }
