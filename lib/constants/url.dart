00c49aeced223bd2ff411d0a3a6693de93eb8a4b
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-21 22:31:56 +0530

    url added.

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -1,11 +1,16 @@
-class Links{
+class Links {
   static const String imagePath = "assets/profile.jpg";
   static const String resumeLink =
-      "https://drive.google.com/file/d/1F_xLidNLIUZJMoQI4VthB7tHWVjExYHk/view?usp=share_link";
-  static const String studentContactLink= "https://github.com/yukthi14/student_contact";
-  static const String dataStructureLink="https://github.com/yukthi14/Data_Structure";
-  static const String calculatorLink="https://github.com/yukthi14/cal";
-  static const String blocConnectionLink="https://github.com/yukthi14/bloc_connection";
-  static const String linkedinLink="https://www.linkedin.com/in/yukthi-j-achar-069778201/";
-  static const String githubLink="https://github.com/yukthi14";
+      "https://drive.google.com/file/d/1JjRRvE9zYbNTz-A-8IVEKhqYk29W6Knv/view?usp=drive_link";
+  static const String studentContactLink =
+      "https://github.com/yukthi14/student_contact";
+  static const String dataStructureLink =
+      "https://github.com/yukthi14/Data_Structure";
+  static const String calculatorLink = "https://github.com/yukthi14/cal";
+  static const String blocConnectionLink =
+      "https://github.com/yukthi14/bloc_connection";
+  static const String linkedinLink =
+      "https://www.linkedin.com/in/yukthi-j-achar-069778201/";
+  static const String githubLink = "https://github.com/yukthi14";
+  static const String twitterLink = "https://twitter.com/YukthiAchar";
 }
