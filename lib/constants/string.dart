04951480dc4cd9fb61790176541f7e1d0565b50d
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-21 22:30:29 +0530

    new strings added.

--- lib/constants/string.dart
+++ lib/constants/string.dart
@@ -4,21 +4,52 @@ class Strings {
   static const String portfolio = "Portfolio";
   static const String home = "Home";
   static const String about = "About";
-  static const String services = "Services";
+  static const String education = "Education";
+  static const String experience = "Experience";
   static const String contact = "Contact";
   static const String helloText = "Hello, It\'s Me";
   static const String role = "And I\'m a ";
+  static const String role1 = "Flutter Developer!";
   static const String downloadCv = "Download CV";
+  static const String me = "Me";
+  static const String collegeName = "AMC Engineering College";
+  static const String courseName = "BE in Information Science and Technology";
+  static const String year = "2020 - 2024";
+  static const String achievement = "Achievement : ";
+  static const String achievementDes =
+      "University rank 33rd and 46th in 4th and 5th semester";
+  static const String companyName1 = "Selfieera Private Limited";
+  static const String companyName2 = "Liveasy Logistics";
+  static const String job = "Flutter Developer Intern";
+  static const String date1 = "April 2023 - June 2023";
+  static const String date2 = "July 2023 - January 2024";
+  static const String des1 = '''
+  * Gained knowledge on state management techniques such as Bloc. 
+  * Firebase integration.
+  * Used authentication with email and phone number.  ''';
+  static const String des2 = '''
+       * Experience in building responsive and interactive user interfaces.
+       * State management techniques such as provider. 
+       * Integrated restful API's.
+       * Deployment of website using github actions and firebase.
+       * Translating figma design and familiar with jira software.''';
+  static const String contactEmail = "yukthiachar@gmail.com";
+  static const String email = "Email";
+  static const String socialProfiles = "Social Profiles";
+  static const String animatedText = "Flutter Developer";
 
   static const String name = "Yukthi J Achar";
-  static const String username = "@yukthi";
-  static const String contactEmail = "yukthiachar@gmail.com";
+
   static const String aboutMeSummary = '''
-* Currently an engineering student with full of enthusiasm and leadership quality, good at problem solving .
-* I easily get adopted to the environment and workspace.
-* Some of the programming languages which i know are dart(flutter) , java , c , c ++ , sql , python .
-* Strong in flutter which uses dart programming, I have done many projects related to flutter in app and website.
-* In intermediate level comes java, all other languages i have a good knowledge about the syntax and usage.
+* Enthusiastic and motivated Flutter Developer with a strong foundation in mobile application development.
+* Proficient in using Dart programming language.
+* Eager to leverage skills and contribute to innovative projects in a dynamic team environment.
+* Committed to staying updated with the latest trends and best practices in Flutter development.
+* Strong communication and collaboration abilities, fostering effective teamwork and project coordination.
+''';
+  static const String dashboardSummary = '''
+* I am a good timekeeper, always willing to learn new skills .
+* I am able to work well both in a team environment. I am also proficient in using my own initiative.
 ''';
   static const String studentContact = "Student Contact";
   static const String stuConDes =
