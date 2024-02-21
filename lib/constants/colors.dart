9100ce5040055afde58ffe8fa3891938114eb69e
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-21 22:24:14 +0530

    black and boxShadow color added.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color bgColor = const Color(0xff003153);
+  static Color bgColor = const Color(0xff09203a);
+  static Color bgColor2 = const Color(0xff0010e1b);
   static Color themeColor = const Color(0xff007BA7);
   static Color splashColor = const Color(0xFF84FFFF);
   static Color borderColor = const Color(0xFF18FFFF);
   static Color white = Colors.white;
   static Color lightBlue = Colors.lightBlue;
+  static Color black = Colors.black54;
+  static Color boxShadow = Colors.lightBlue.shade800;
   static Color lightShadeBlue = Colors.lightBlue.shade200;
 }
