8a69c5f7ef989e1901a4f75d4ad52f7498c6f685
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-22 20:20:34 +0530

    firebase initialization in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/screens/dashboard.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
