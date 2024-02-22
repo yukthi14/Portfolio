2777eaa01dfe50ea908cc11f4bd0072b807913da
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2024-02-22 19:42:41 +0530

    flutter, dart, firebase, postman, androidStudio, vs code logo added.

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -4,4 +4,9 @@ class AppAssets {
   static String twitter = 'assets/twit.png';
   static String profileImage = 'assets/profile.jpg';
   static String amcLogo = 'assets/amc_logo.png';
+  static String flutterDartLogo = 'assets/flutter_dart_logo.png';
+  static String firebaseLogo = 'assets/firebase_logo.png';
+  static String postmanLogo = 'assets/postman_logo.png';
+  static String androidStudioLogo = 'assets/android_studio_logo.png';
+  static String vsCodeLogo = 'assets/vs_code_logo.jpg';
 }
